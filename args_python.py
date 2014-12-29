a2fcd394e82e0122aadf04c9e64080e2e4b6aa1e
Zhu Yun <yzhu.andrew@163.com>
2014-12-29 16:53:33 +0800

    saved

---
+++ args_python.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Yun'
+__project__ = 'WebPythonTest'
+
+
+# def print_args(*strs):
+#     for each in strs:
+#         print(each)
+#
+# strs = ('abc', 'dddd')
+#
+# print_args(strs)
+
+import random
+print(random.choice(range(100)))
