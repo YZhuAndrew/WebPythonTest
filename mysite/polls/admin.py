5bc9ecaa8e4486c1d478a2df2d5fd4df035648ff
Zhu Yun <yzhu.andrew@163.com>
2014-12-31 16:21:14 +0800

    create a django project -create polls app

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 
-from polls.models import Question
+from polls.models import Question, Choice
+
+# admin.site.register(Choice)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -10,6 +17,10 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,                  {'fields': ['question_text']}),
         ('Date information',    {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 
 admin.site.register(Question, QuestionAdmin)
